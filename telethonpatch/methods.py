4faae0ae173823eae2180208949985dbd268165a
lanowde <117498666+lanowde@users.noreply.github.com>
2023-09-08 18:17:27 +0530

    unecessary

--- telethonpatch/methods.py
+++ telethonpatch/methods.py
@@ -280,5 +280,5 @@ setattr(TelegramClient, "get_topics", get_topics)
 setattr(TelegramClient, "join_chat", join_chat)
 setattr(TelegramClient, "toggle_hidden", toggle_hidden)
 
-setattr(TelegramClient, "set_profile_photo", set_profile_photo)
+# setattr(TelegramClient, "set_profile_photo", set_profile_photo)
 setattr(TelegramClient, "set_contact_photo", set_contact_photo)
